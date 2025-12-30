0e3936fc8885a096c68fdaddccd1d5511a386aa1
Ryan Luu <ryan.luu@ryanluu.dev>
2025-12-29 22:44:58 -0800

    Fix package scripts
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- just.config.ts
+++ just.config.ts
@@ -1,15 +1,12 @@
-import { argv, parallel, series, task, tscTask } from "just-scripts";
+import { argv, parallel, series, task } from "just-scripts";
 import {
-  BundleTaskParameters,
   CopyTaskParameters,
-  bundleTask,
   cleanTask,
   cleanCollateralTask,
   copyTask,
   coreLint,
-  mcaddonTask,
   setupEnvironment,
-  ZipTaskParameters,
+  zipTask,
   STANDARD_CLEAN_PATHS,
   DEFAULT_CLEAN_DIRECTORIES,
   getOrThrowFromProcess,
@@ -18,38 +15,20 @@ import {
 import path from "path";
 setupEnvironment(path.resolve(__dirname, ".env"));
 const projectName = getOrThrowFromProcess("PROJECT_NAME");
-const bundleTaskOptions: BundleTaskParameters = {
-  entryPoint: path.join(__dirname, "./scripts/main.ts"),
-  external: ["@minecraft/server", "@minecraft/server-ui"],
-  outfile: path.resolve(__dirname, "./dist/scripts/main.js"),
-  minifyWhitespace: false,
-  sourcemap: true,
-  outputSourcemapPath: path.resolve(__dirname, "./dist/debug"),
-};
 const copyTaskOptions: CopyTaskParameters = {
-  copyToBehaviorPacks: [`./behavior_packs/${projectName}`],
-  copyToScripts: ["./dist/scripts"],
+  copyToBehaviorPacks: [],
+  copyToScripts: [],
   copyToResourcePacks: [`./resource_packs/${projectName}`],
 };
-const mcaddonTaskOptions: ZipTaskParameters = {
-  ...copyTaskOptions,
-  outputFile: `./dist/packages/${projectName}.mcaddon`,
-};
-task("lint", coreLint(["scripts/**/*.ts"], argv().fix));
-task("typescript", tscTask());
-task("bundle", bundleTask(bundleTaskOptions));
-task("build", series("typescript", "bundle"));
+task("lint", coreLint(["resource_packs/**/*.json"], argv().fix));
 task("clean-local", cleanTask(DEFAULT_CLEAN_DIRECTORIES));
 task("clean-collateral", cleanCollateralTask(STANDARD_CLEAN_PATHS));
 task("clean", parallel("clean-local", "clean-collateral"));
 task("copyArtifacts", copyTask(copyTaskOptions));
 task("package", series("clean-collateral", "copyArtifacts"));
+task("local-deploy", watchTask(["resource_packs/**/*.{json,lang,tga,ogg,png}"], series("clean-local", "package")));
 task(
-  "local-deploy",
-  watchTask(
-    ["scripts/**/*.ts", "behavior_packs/**/*.{json,lang,tga,ogg,png}", "resource_packs/**/*.{json,lang,tga,ogg,png}"],
-    series("clean-local", "build", "package")
-  )
+  "createMcpackFile",
+  zipTask(`./dist/packages/${projectName}.mcpack`, [{ contents: [`./resource_packs/${projectName}`] }])
 );
-task("createMcaddonFile", mcaddonTask(mcaddonTaskOptions));
-task("mcaddon", series("clean-local", "build", "createMcaddonFile"));
+task("mcpack", series("clean-local", "createMcpackFile"));
